6a860360a2e405e5a3f7144b8c4b0fe206e0ffc2
rbnis <5955614+rbnis@users.noreply.github.com>
2021-12-05 00:51:39 +0100

    feat: Add switch to turn off lights

--- site/flat/hallway/switch.ts
+++ site/flat/hallway/switch.ts
@@ -1,16 +1,47 @@
-import { mqttSensor } from 'haah';
+import { mqttSensor, updateState } from 'haah';
 import { switchToggleMotionOverwrite } from '../../../util/motion';
+import { taints } from '../../../util/enums'
 
 import { hallway } from ".";
+import { kitchen } from "../kitchen";
+import { livingroom } from "../livingroom";
+import { bedroom } from "../bedroom";
 
 let overwriteTimeout: NodeJS.Timeout;
 mqttSensor('zigbee2mqtt/switch/hallway_door_1', (payload) => {
   if (payload.action === 'left_press') {
-    switchToggleMotionOverwrite(hallway, overwriteTimeout, 5, 60 * 30)
+    switchToggleMotionOverwrite(hallway, overwriteTimeout, 5, 60 * 30);
   }
 });
 mqttSensor('zigbee2mqtt/switch/hallway_door_2', (payload) => {
   if (payload.action === 'left_press') {
-    switchToggleMotionOverwrite(hallway, overwriteTimeout, 5, 60 * 30)
+    switchToggleMotionOverwrite(hallway, overwriteTimeout, 5, 60 * 30);
+  }
+});
+
+mqttSensor('zigbee2mqtt/switch/hallway_entry', (payload) => {
+  if (payload.action === 'off') {
+    updateState(bedroom, (state) => {
+      state.lightOn = false;
+    });
+    updateState(livingroom, (state) => {
+      state.lightOn = false;
+    });
+    updateState(hallway, (state) => {
+      state.overwrites.ceiling = taints.lightOff;
+    });
+    updateState(kitchen, (state) => {
+      state.overwrites.ceiling = taints.lightOff;
+    });
+
+    if (overwriteTimeout) clearTimeout(overwriteTimeout);
+    overwriteTimeout = setTimeout(() => {
+      updateState(hallway, (state) => {
+        state.overwrites.ceiling = taints.none;
+      });
+      updateState(kitchen, (state) => {
+        state.overwrites.ceiling = taints.none;
+      });
+    }, 1000 * 60 * 5);
   }
 });
