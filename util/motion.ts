369affb789cd6b47e8c1b697cf35de2e3eb8cdca
rbnis <5955614+rbnis@users.noreply.github.com>
2021-12-07 11:04:21 +0100

    fix: React on first use under certain conditions

--- util/motion.ts
+++ util/motion.ts
@@ -1,15 +1,20 @@
 import { updateState } from "haah";
+import { environment } from '../site/environment';
 import { taints } from "./enums";
 
 export function switchToggleMotionOverwrite(state: any, timeout: NodeJS.Timeout, secondsOff: number, secondsOn: number) {
   var overwriteTime = 0;
+  var currentlyOn = true;
+
   updateState(state, (state) => {
-    overwriteTime = state.overwrites.ceiling === taints.lightOn ? secondsOff : secondsOn;
-    state.occupied = state.overwrites.ceiling === taints.lightOn ? false : state.occupied;
-    state.overwrites.ceiling = state.overwrites.ceiling === taints.lightOn
-      || (state.occupied && state.overwrites.ceiling === taints.none)
-        ? taints.lightOff
-        : taints.lightOn;
+    if (state.overwrites.ceiling === taints.lightOff) currentlyOn = false;
+    else if (state.overwrites.ceiling === taints.lightOn) currentlyOn = true;
+    else if (!state.occupied ) currentlyOn = false;
+    else if (environment.daylight) currentlyOn = false;
+
+    overwriteTime = currentlyOn ? secondsOff : secondsOn;
+    state.occupied = currentlyOn ? false : state.occupied;
+    state.overwrites.ceiling = currentlyOn ? taints.lightOff : taints.lightOn;
   });
 
   if (timeout) clearTimeout(timeout);
